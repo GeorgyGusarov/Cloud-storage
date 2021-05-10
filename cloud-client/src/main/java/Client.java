b8ce4f451e47830d2de92e19fe29d0495e918c1c
Georgy <gogalsh@gmail.com>
2021-05-10 22:21:15 +0300

    minor changes

--- cloud-client/src/main/java/Client.java
+++ cloud-client/src/main/java/Client.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
+        File file = new File("C:\\Programming\\Cloud storage\\cloud-client\\src\\main\\resources\\pepe.jpg");
+        //System.out.println(file.exists());
 
+        try (Socket socket = new Socket("localhost", 8189);) {
+            System.out.println("Connected to server");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeUTF("pepe.jpg");
+            out.writeLong(file.length());
+
+            FileInputStream fis = new FileInputStream(file);
+            int x;
+            while ((x = fis.read()) != -1) {
+                out.write(x);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- cloud-server/src/main/java/Server.java
+++ cloud-server/src/main/java/Server.java
@@ -3,26 +3,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8189);
-        Socket socket = serverSocket.accept();
-        System.out.println("Client accepted");
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(8189);
+            Socket socket = serverSocket.accept();
+            System.out.println("Client accepted");
 
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-        int length = in.readInt();
-        System.out.println("fileLength: " + length);
-        String fileName = in.readUTF();
+            long length = in.readLong();
+            System.out.println("fileLength: " + length);
+            String fileName = in.readUTF();
 
-        File file = new File(fileName);
-        if (!file.exists()) file.createNewFile();
+            File file = new File(fileName);
+            if (!file.exists()) file.createNewFile();
 
-        FileOutputStream fos = new FileOutputStream(file);
-        for (int i = 0; i < length; i++) {
-            fos.write(in.read());
+            FileOutputStream fos = new FileOutputStream(file);
+            for (long i = 0; i < length; i++) {
+                fos.write(in.read());
+                if (i % 1000 == 0) System.out.println(10000 + " b downloaded.");
+            }
+            fos.close();
+            out.writeUTF("File: " + fileName + ", downloaded!");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        fos.close();
-        out.writeUTF("File: " + fileName + ", downloaded!");
     }
 }
