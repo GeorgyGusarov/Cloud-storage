a46216d3ce0c1d266ecc02e69dea70e4730dd93b
Georgy <gogalsh@gmail.com>
2021-06-09 22:55:36 +0300

    added more code

--- cloud-server/src/main/java/java_nio_examples/PathTest.java
+++ cloud-server/src/main/java/java_nio_examples/PathTest.java
@@ -1,20 +1,27 @@
 package java_nio_examples;
 
+import java.io.IOException;
 import java.nio.file.*;
 
 public class PathTest {
-    public static void main(String[] args) {
-        Path path = Paths.get("1", "2", "1.txt");
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("PathTest.txt");
+
+        if (!Files.exists(Paths.get("PathTest_Dir"))) Files.createDirectory(Paths.get("PathTest_Dir"));
+        Files.write(path, new byte[]{65, 66, 67}, StandardOpenOption.CREATE);
+        Files.copy(path, Paths.get("PathTest_Dir", "PathTest_copy.txt"), StandardCopyOption.REPLACE_EXISTING);
+
         System.out.println(path);
         System.out.println(path.startsWith(Paths.get("1", "2")));
         System.out.println(Files.exists(path));
         System.out.println(path.getFileName());
         System.out.println(path.getFileSystem());
-        System.out.println(path.getParent());
-        System.out.println(path.getParent().getParent());
-        System.out.println(path.getParent().getParent().getParent());
+        System.out.println(path.resolve(Paths.get("PathTest_Dir", "PathTest_copy.txt")));
 
-
-
+        Path absPath = path.toAbsolutePath();
+        while (absPath.getParent() != null) {
+            System.out.println(absPath);
+            absPath = absPath.getParent();
+        }
     }
 }
