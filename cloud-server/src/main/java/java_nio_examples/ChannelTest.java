f05823d7ac1f9158bbe97456100a756e40d1f07a
Georgy <gogalsh@gmail.com>
2021-06-20 22:59:13 +0300

    more code

---
+++ cloud-server/src/main/java/java_nio_examples/ChannelTest.java
@@ -0,0 +1,21 @@
+package java_nio_examples;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+
+public class ChannelTest {
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile file = new RandomAccessFile("FileTest.txt", "rw");
+        FileChannel channel = file.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(5);
+        for (int i = 0; i < 5; i++) {
+            buffer.put((byte) (65 + i));
+        }
+        System.out.println(Arrays.toString(buffer.array()));
+        buffer.flip();
+        channel.write(buffer);
+    }
+}
--- cloud-server/src/main/java/java_nio_examples/PathTest.java
+++ cloud-server/src/main/java/java_nio_examples/PathTest.java
@@ -11,17 +11,25 @@ public class PathTest {
         Files.write(path, new byte[]{65, 66, 67}, StandardOpenOption.CREATE);
         Files.copy(path, Paths.get("PathTest_Dir", "PathTest_copy.txt"), StandardCopyOption.REPLACE_EXISTING);
 
-        System.out.println(path);
-        System.out.println(path.startsWith(Paths.get("1", "2")));
-        System.out.println(Files.exists(path));
-        System.out.println(path.getFileName());
-        System.out.println(path.getFileSystem());
-        System.out.println(path.resolve(Paths.get("PathTest_Dir", "PathTest_copy.txt")));
+        System.out.println(path + "\n");
+        System.out.println(path.startsWith(Paths.get("1", "2")) + "\n");
+        System.out.println(Files.exists(path) + "\n");
+        System.out.println(path.getFileName() + "\n");
+        System.out.println(path.getFileSystem() + "\n");
+        System.out.println(path.resolve(Paths.get("PathTest_Dir", "PathTest_copy.txt")) + "\n");
 
         Path absPath = path.toAbsolutePath();
         while (absPath.getParent() != null) {
             System.out.println(absPath);
             absPath = absPath.getParent();
         }
+        System.out.println();
+
+        // Path итерируемый, поэтому можно через foreach
+        Path p = Paths.get("PathTest_Dir");
+        for (Path tmp : p) {
+            System.out.println(tmp);
+        }
+        System.out.println();
     }
 }
