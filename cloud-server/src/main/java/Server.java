aeb808490877da45347e1880be52d51c323b75e0
Georgy <gogalsh@gmail.com>
2021-07-11 21:53:49 +0300

    removing deleted files from tracking

--- cloud-server/src/main/java/Server.java
+++ cloud-server/src/main/java/Server.java
@@ -1,35 +1,40 @@
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.channel.socket.*;
 
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Waiting for client to connect...");
-        ServerSocket serverSocket = null;
+    private final static int PORT = 8189;
+
+    private void run() throws Exception {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        System.out.println("Server online");
+
         try {
-            serverSocket = new ServerSocket(8189);
-            Socket socket = serverSocket.accept();
-            System.out.println("Client " + serverSocket.getLocalPort() + " accepted");
-
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            String fileName = in.readUTF();
-            long length = in.readLong();
-            System.out.println("fileLength: " + length + " Mb.");
-
-            File file = new File(fileName);
-            if (!file.exists()) file.createNewFile();
-
-            FileOutputStream fos = new FileOutputStream(file);
-            byte[] buffer = new byte[30_000];
-            for (long i = 0; i < length; i++) fos.write(in.read());
-
-            fos.close();
-            out.writeUTF("File: " + fileName + ", downloaded!");
-        } catch (IOException e) {
-            e.printStackTrace();
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(mainGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline().addLast(new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+                                                             new ObjectEncoder(),
+                                                             new ServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 }
